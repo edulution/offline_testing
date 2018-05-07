dd2864e895f28aa5a04ed23ba475dba12315ad70
Kapya <kapsakala@gmail.com>
2018-05-07 12:10:16 +0200

    added function to read users list from csv file and make js objects

--- index.js
+++ index.js
@@ -15,6 +15,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 /*Serve static assets from the public folder*/
 app.use(express.static('public'));
 
+/*Get users from csv file and store as arraylist*/
+/*===========*/
+/*require filestream and csv libraries*/
+var fs = require('fs');
+var csv = require('csv');
+
+/*instantiate empty users array*/
+var users = [];
+
+/*read users csv file with headers*/
+var readStream = fs.createReadStream('users.csv.headers');
+
+/*parse by columns and push objects to users array*/
+var parser = csv.parse({columns:true});
+
+parser.on('readable', function() {
+  while(record = parser.read()) {
+    users.push(record);
+  }
+});
+
+/*log error messages*/
+parser.on('error', function(err) {
+  console.log(err.message);
+});
+
+/*log users array when finished sucessfully*/
+parser.on('finish', (function() {
+  console.log(users);
+}));
+
+readStream.pipe(parser);
+/*=======*/
+
 /*Get today's date in the form yyyy-mm-dd*/
 var today = new Date();
 var dd = today.getDate();
@@ -36,6 +70,12 @@ today = "'"+ yyyy + '-' + mm + '-' + dd +"'";
 /*Single form for demonstration. Loaded when root of server requested*/
 app.get('', function (req, res) {
  res.sendFile( __dirname + '/form1.html');
+});
+
+
+/*endpoint to get users list as json*/
+app.get('/get_users',function(req, res){
+        return res.json(users);
 });
 
 
