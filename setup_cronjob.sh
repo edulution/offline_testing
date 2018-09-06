30d33f09cd4395be74054491f42fdc7d34fb339a
Kapya <kapsakala@gmail.com>
2018-09-06 15:24:10 +0200

    cron job to repopulate users table on startup or reboot and start testing server

---
+++ setup_cronjob.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# check if cronjob exists for startup_script and add it if not
+# search for cronjob which matches @reboot ~/.baseline_testing/startup_script 
+if (crontab -l | grep -q '@reboot ~/.baseline_testing/startup_script'); then
+	#skip if already exists
+	echo 'Cronjob already exists. Skipping ....'
+else
+	#add entry if doesnt exist
+	echo 'Cronjob does not exist. Creating now'
+	(crontab -l 2>/dev/null; echo "@reboot ~/.baseline_testing/startup_script") | crontab -
+fi
--- start_testing_server.sh
+++ start_testing_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Restart testing server
-forever stopall
-forever start ~/.baseline_testing/index.js
-forever logs 0
+forever_path=/usr/local/bin/forever
+node_path=/usr/local/bin/node
+
+$forever_path -c $node_path stopall
+$forever_path start -c $node_path ~/.baseline_testing/index.js
+$forever_path -c $node_path logs 0
