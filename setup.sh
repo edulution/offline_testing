613c16125bf5fff07c7ef897bd0bf18f87c437d8
Kapya <kapsakala@gmail.com>
2018-09-14 10:09:28 +0200

    link npm packages when doing setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # First time setup script
+# Go into directory for baseline testing
+cd ~/.baseline_testing/
+
+# Link npm packages
+npm link sqlite3
+npm link csv
+npm link body-parser
+npm link express
 
 #set up responses db and get users from KA
 ~/.baseline_testing/get_users_and_device.sh
