8804cbb44cbe507bf8a39b6e578b5a51a5701e70
Kapya <kapsakala@gmail.com>
2018-10-15 11:50:41 +0200

    create table test_marks

---
+++ create_test_marks.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS test_marks(
+	test varchar unique not null,
+	testmaxscore integer
+);
+
+insert into test_marks (test, testmaxscore) values ('alp_comp_1',50);
+insert into test_marks (test, testmaxscore) values ('alp_comp_2',50);
+insert into test_marks (test, testmaxscore) values ('alp_sec_a1',25);
+insert into test_marks (test, testmaxscore) values ('alp_sec_a2',25);
+insert into test_marks (test, testmaxscore) values ('alp_sec_b1',25);
+insert into test_marks (test, testmaxscore) values ('alp_sec_b2',25);
+insert into test_marks (test, testmaxscore) values ('alp_sec_c1',25);
+insert into test_marks (test, testmaxscore) values ('alp_sec_c2',25);
+insert into test_marks (test, testmaxscore) values ('alp_sec_d1',25);
+insert into test_marks (test, testmaxscore) values ('alp_sec_d2',25);
+insert into test_marks (test, testmaxscore) values ('prealp_comp_1',50);
+insert into test_marks (test, testmaxscore) values ('prealp_comp_2',50);
+insert into test_marks (test, testmaxscore) values ('prealp_comp_3',50);
+insert into test_marks (test, testmaxscore) values ('lit_alp_a1',50);
+insert into test_marks (test, testmaxscore) values ('lit_alp_a2',50);
+insert into test_marks (test, testmaxscore) values ('lit_alp_b1',50);
+insert into test_marks (test, testmaxscore) values ('lit_alp_b2',50);
+insert into test_marks (test, testmaxscore) values ('lit_alp_c1',50);
+insert into test_marks (test, testmaxscore) values ('lit_alp_c2',50);
+insert into test_marks (test, testmaxscore) values ('lit_alp_d1',60);
+insert into test_marks (test, testmaxscore) values ('lit_alp_d2',60);
+insert into test_marks (test, testmaxscore) values ('lit_prealp_1',40);
+insert into test_marks (test, testmaxscore) values ('lit_prealp_2',40);
