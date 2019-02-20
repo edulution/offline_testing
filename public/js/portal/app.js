abf979c7265442239b54c932f344ec4e18e14f1e
Kapya Sakala <kapsakala@gmail.com>
2019-02-20 08:47:00 +0200

    home page for portal

--- public/js/portal/app.js
+++ public/js/portal/app.js
@@ -1,1 +1,5 @@
-angular.module('edulutionPortal', ['ngAnimate', 'ngSanitize', 'ui.bootstrap','ui', 'ui.filters']);
+angular.module('edulutionPortal', [])
+.controller('MainCtrl', ['$scope', function ($scope) {
+$scope.topics = ['Fractions','Fractions','Fractions','Fractions','Fractions','Fractions','Fractions','Fractions','Fractions','Fractions','Fractions','Fractions','Decimals','Decimals','Decimals','Decimals','Decimals','Decimals','Decimals','Decimals','Decimals','Decimals','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Geometry','Factors and Multiples','Factors and Multiples','Factors and Multiples','Combined Operations','Combined Operations','Combined Operations'];
+console.log($scope.topics);
+}]);
