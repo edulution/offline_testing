a4b5f4a4546d2dd2d4db3034f319d9bfd340186a
Kapya Sakala <kapsakala@gmail.com>
2019-05-17 14:18:25 +0200

    press enter to submit password on modal

--- public/pmodule/app.js
+++ public/pmodule/app.js
@@ -244,6 +244,21 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap','ui', '
    };
 
 /*end whitelist directive*/
+})
+
+/*directive to capture the keypress of the Enter key*/
+.directive('pressEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.pressEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
 });
 
 
