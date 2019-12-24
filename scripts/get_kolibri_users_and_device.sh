d0c97d5492554ae841b6d12f4a6db5898532a6d0
Kapya <kapsakala@gmail.com>
2019-12-24 14:19:41 +0200

    exit immediately if database does not exist

--- scripts/get_kolibri_users_and_device.sh
+++ scripts/get_kolibri_users_and_device.sh
@@ -17,28 +17,29 @@ export reset=`tput sgr0`
 export bold=`tput bold`
 
 # check if test responses db exists. If not, create it
-db_exists=$( check_db_exists baseline_testing )
+db_exists=$( check_db_exists $BASELINE_DATABASE_NAME )
 
 scripts_dir=~/.baseline_testing/sql/
 
 if [[ "$dbexists" == "Database Exists" ]]; then
 	# Let the user know that the database already exists and skip
 	echo "${blue}Database already exists.Skipping...${reset}"
-else
-	echo "${green}Database does not exist. Creating now...${reset}"
-	# create database for baseline testing
-	sudo -u postgres bash -c "createdb baseline_testing"
+elif [[ "$dbexists" == "Database does not exist!" ]]; then
+	echo "${red}Database does not exist. Creating now...${reset}"
+	exit 1
+	# # create database for baseline testing
+	# sudo -u postgres bash -c "createdb baseline_testing"
 	
-	# create role for the database and grant permissions
-	sudo -u postgres bash -c "psql -c \"CREATE USER $BASELINE_DATABASE_USER WITH PASSWORD '$BASELINE_DATABASE_PASSWORD';\""
-	sudo -u postgres bash -c "psql -c \"GRANT ALL ON DATABASE $BASELINE_DATABASE_NAME TO $BASELINE_DATABASE_USER;\""
+	# # create role for the database and grant permissions
+	# sudo -u postgres bash -c "psql -c \"CREATE USER $BASELINE_DATABASE_USER WITH PASSWORD '$BASELINE_DATABASE_PASSWORD';\""
+	# sudo -u postgres bash -c "psql -c \"GRANT ALL ON DATABASE $BASELINE_DATABASE_NAME TO $BASELINE_DATABASE_USER;\""
 
-	# run table/view/function creation scripts
-	# loop through the sql files in the scripts directory and execute each one
-	echo "${green}Creating database tables...${reset}"
-	for file in $scripts_dir/*.sql; do
-		psql postgresql://$BASELINE_DATABASE_USER:$BASELINE_DATABASE_PASSWORD@$BASELINE_DATABASE_HOST/$BASELINE_DATABASE_NAME -a -f $file
-	done
+	# # run table/view/function creation scripts
+	# # loop through the sql files in the scripts directory and execute each one
+	# echo "${green}Creating database tables...${reset}"
+	# for file in $scripts_dir/*.sql; do
+	# 	psql postgresql://$BASELINE_DATABASE_USER:$BASELINE_DATABASE_PASSWORD@$BASELINE_DATABASE_HOST/$BASELINE_DATABASE_NAME -a -f $file
+	# done
 fi
 
 # get device name and store it in csv file
