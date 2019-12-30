ad40b213a2f2d8ffa224acfea942a08b31927fb9
Kapya <kapsakala@gmail.com>
2019-12-30 09:30:28 +0200

    function to check if database exists

--- scripts/check_db_exists.sh
+++ scripts/check_db_exists.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 # Function to check whether a database exists
 # The kolibri database user can be used to check for other databases on the server too
-function check_db_exists(){
-	if psql postgresql://$KOLIBRI_DATABASE_USER:$KOLIBRI_DATABASE_PASSWORD@$KOLIBRI_DATABASE_HOST -lqt | cut -d \| -f 1 | grep -qw $1;then
-	    # database exists
-	    # $? is 0
-	    echo "Database Exists"
-	else
-	    # ruh-roh
-	    # $? is 1
-	    echo "Database does not exist!"
-	fi
+#!/bin/bash
+function db_exists() {
+  psql -lqt | cut -d \| -f 1 | grep -wq $1
 }
-
-check_db_exists $1
