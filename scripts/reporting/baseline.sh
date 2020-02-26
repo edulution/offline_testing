148c24c8ef049025f3c9adb544cbf33b42596931
Kapya Sakala <kapsakala@gmail.com>
2020-02-26 21:47:10 +0200

    removed color vars. slight changes to comments

--- scripts/reporting/baseline.sh
+++ scripts/reporting/baseline.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
+# source a helper function to check if a database exists
 source ~/.baseline_testing/scripts/check_db_exists.sh
-
-#colors
-#=======
-export black=`tput setaf 0`
-export red=`tput setaf 1`
-export green=`tput setaf 2`
-export yellow=`tput setaf 3`
-export blue=`tput setaf 4`
-export magenta=`tput setaf 5`
-export cyan=`tput setaf 6`
-export white=`tput setaf 7`
-
-# reset to default bash text style
-export reset=`tput sgr0`
-
-# make actual text bold
-export bold=`tput bold`
-
-# make background color on text
-export bold_mode=`tput smso`
-
-# remove background color on text
-export exit_bold_mode=`tput rmso`
 
 # check if directories exist
 DIRECTORIES=( ~/.reports/baseline )
@@ -35,16 +13,16 @@ for DIRECTORY in ${DIRECTORIES[@]}; do
   fi
 done
 
-#pull latest changes from master branch in repo
+# pull latest changes from master branch in repo
 cd ~/.baseline_testing
 git reset --hard origin/zambia > /dev/null
 git pull origin zambia > /dev/null
 
-#check if database file exists before extracting reports
+# check if database file exists before extracting reports
 if db_exists $BASELINE_DATABASE_NAME ; then
   # Let the user know that the database already exists and skip
   echo "${blue}Database already exists.Skipping...${reset}"
-  #if db file exists then extraction and submission begin. If not, will output error message to contact support
+  # if db file exists then extraction and submission begin. If not, will output error message to contact support
 	if (echo $1 |\
     egrep '^(1[0-2]|0[0-9])[-/][0-9]{2}' > /dev/null
 	); then
