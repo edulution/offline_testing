eb5379448f075f18653fbeef5dad4c73dfe62ef0
Kapya <kapsakala@gmail.com>
2020-06-01 21:09:48 +0200

    created routes for grade7 and literacy

--- routes/grade7/gr7_numeracy.js
+++ routes/grade7/gr7_numeracy.js
@@ -10,74 +10,8 @@ router.use(express.static(path.resolve('public')));
 /*It is assumed that the name of the html file is the same as the requested test*/
 router.get('/:test', (req, res) => {
 	selected_test = req.params.test
-	res.sendFile( path.resolve('grade_7/numeracy/'+ selected_test + '.html'));
+	/* Construct path to Grade 7 revision numeracy */
+	res.sendFile( path.resolve('grade7/numeracy/'+ selected_test + '.html'));
 });
 
 module.exports = router;
-
-/*Grade 7 revision*/
-
-/*grade 7 mock tests*/
-/*app.get('/gr7_mock1', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_mock1.html');
-});
-app.get('/gr7_mock2', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_mock2.html');
-});
-
-app.get('/gr7_test1', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test1.html');
-});
-
-app.get('/gr7_test2', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test2.html');
-});
-
-app.get('/gr7_test3', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test3.html');
-});
-
-app.get('/gr7_test4', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test4.html');
-});
-
-app.get('/gr7_test5', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test5.html');
-});
-
-app.get('/gr7_test6', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test6.html');
-});
-
-app.get('/gr7_test7', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test7.html');
-});
-
-app.get('/gr7_test8', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test8.html');
-});
-
-app.get('/gr7_test9', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test9.html');
-});
-
-app.get('/gr7_test10', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test10.html');
-});
-
-app.get('/gr7_test11', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test11.html');
-});
-
-app.get('/gr7_test12', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test12.html');
-});
-
-app.get('/gr7_test13', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test13.html');
-});
-
-app.get('/gr7_test14', (req, res) => {
- res.sendFile( __dirname + '/grade_7/grade7_test14.html');
-});
-*/
--- routes/literacy/literacy.js
+++ routes/literacy/literacy.js
@@ -1,40 +1,17 @@
-/*Litercy tests*/
-app.get('/lit_alpha_a1', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_a1.html');
+const express = require('express')
+const router = express.Router()
+const path = require('path');
+
+/*Use the public directory to serve static assets*/
+router.use(express.static(path.resolve('public'))); 
+
+/*Get the selected test as a param of the request*/
+/*then construct the path to the html file*/
+/*It is assumed that the name of the html file is the same as the requested test*/
+router.get('/:test', (req, res) => {
+	selected_test = req.params.test
+	/* Construct path to Grade 7 revision numeracy */
+	res.sendFile( path.resolve('literacy/'+ selected_test + '.html'));
 });
 
-app.get('/lit_alpha_a2', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_a2.html');
-});
-
-app.get('/lit_alpha_b1', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_b1.html');
-});
-
-app.get('/lit_alpha_b2', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_b2.html');
-});
-
-app.get('/lit_alpha_c1', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_c1.html');
-});
-
-app.get('/lit_alpha_c2', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_c2.html');
-});
-
-app.get('/lit_alpha_d1', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_d1.html');
-});
-
-app.get('/lit_alpha_d2', (req, res) => {
- res.sendFile( __dirname + '/literacy/alpha_d2.html');
-});
-
-app.get('/lit_prealpha_1', (req, res) => {
- res.sendFile( __dirname + '/literacy/prealpha_1.html');
-});
-
-app.get('/lit_prealpha_2', (req, res) => {
- res.sendFile( __dirname + '/literacy/prealpha_2.html');
-});
+module.exports = router;
