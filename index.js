e92188b69b7c1af10596dda1a54a9e68628d4b28
Kapya <kapsakala@gmail.com>
2020-06-02 14:59:03 +0200

    use new consolidated grade7 route

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ const api = require('./routes/api')
 const playlists = require('./routes/playlists')
 const numeracy = require('./routes/numeracy')
 const literacy = require('./routes/literacy')
-const gr7_numeracy = require('./routes/grade7/gr7_numeracy.js')
+const grade7 = require('./routes/grade7')
 
 app.use('/', portal);
 app.use('/api', api);
 app.use('/playlists', playlists);
 app.use('/numeracy', numeracy);
 app.use('/literacy', literacy);
-app.use('/grade7_numeracy', gr7_numeracy);
+app.use('/grade7', grade7);
 
 /*Results dashboard page*/
 app.get('/coach', (req, res) => {
