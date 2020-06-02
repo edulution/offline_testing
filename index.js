c6ee465fb124d60c34fb23673379b7ad435c0b01
Kapya <kapsakala@gmail.com>
2020-06-02 02:57:46 +0200

    implemented routes for playlists

--- index.js
+++ index.js
@@ -5,36 +5,33 @@ const path = require('path');
 /*Port the server will run on*/
 const port = 8888;
 
-/*Use bodyParser to parse form data*/ 
+/*Use bodyParser to parse form data*/
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 /*Serve static assets from the public folder*/
-app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
-/*const global.__basedir = __dirname;*/
-
-const api = require('./routes/api.js')
-const numeracy = require('./routes/numeracy.js')
-const literacy = require('./routes/literacy.js')
+const portal = require('./routes/portal')
+const api = require('./routes/api')
+const playlists = require('./routes/playlists')
+const numeracy = require('./routes/numeracy')
+const literacy = require('./routes/literacy')
 const gr7_numeracy = require('./routes/grade7/gr7_numeracy.js')
 
+app.use('/', portal);
+app.use('/api', api);
+app.use('/playlists', playlists);
 app.use('/numeracy', numeracy);
 app.use('/literacy', literacy);
 app.use('/grade7_numeracy', gr7_numeracy);
 
-
-/*portal homepage*/
-app.get('/',(req, res) => {
- res.sendFile( __dirname + '/portal/index.html');
-});
-
 /*Results dashboard page*/
 app.get('/coach', (req, res) => {
- res.sendFile( __dirname + '/dashboard/index.html');
+    res.sendFile(__dirname + '/dashboard/index.html');
 });
 
 app.listen(port, '0.0.0.0', () => {
- console.log('Server running on port ' + port);
+    console.log('Server running on port ' + port);
 });
