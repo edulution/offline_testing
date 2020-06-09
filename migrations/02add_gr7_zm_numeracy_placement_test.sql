1d0fd7c32e04efa46855a3f76d815daf57ab70fa
Kapya <kapsakala@gmail.com>
2020-06-09 12:17:18 +0200

    ordered sql scripts for changes to existing database

---
+++ migrations/02add_gr7_zm_numeracy_placement_test.sql
@@ -0,0 +1,3 @@
+# Add Zambia Grade 7 Numeracy placement test to test_marks
+INSERT INTO test_marks (test_id, test_name, course, module, testmaxscore, test_pass_score, channel_id)
+VALUES('grade7_test15', 'Grade 7 - Placement Test', 'zm_gr7_revision', 'numeracy',30, 0.75, '8d368058656544e2b7fe62eb2a632698');
