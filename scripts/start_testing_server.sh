052fd90859def4baad27efcdad72ad2d3fdca61f
Kapya <kapsakala@gmail.com>
2020-07-12 23:11:09 +0200

    made scripts excecutable. use vars exported from bash colors

--- scripts/start_testing_server.sh
+++ scripts/start_testing_server.sh
@@ -6,6 +6,6 @@ node_path=/usr/local/bin/node
 
 $forever_path -c $node_path stopall > /dev/null
 
-cd ~/.baseline_testing
+cd ~/.baseline_testing || exit
 $forever_path start -c $node_path index.js > /dev/null
 $forever_path -c $node_path logs 0 > /dev/null
