de2ac33ea41206d060b2c88d9f850b23f597b218
Kapya <kapsakala@gmail.com>
2020-07-25 22:39:23 +0200

    added comments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # First time setup script
+
 # Go into directory for baseline testing
 cd ~/.baseline_testing || exit
 
 # Link npm packages
+echo "Linking npm packages"
 npm link pg > /dev/null
 npm link csv > /dev/null
 npm link body-parser > /dev/null
 npm link express > /dev/null
 
 # open up port for server
-sudo ufw allow 8888 > /dev/null
+echo "Opening up port for external access"
+sudo ufw allow 8888
 
-#set up responses db and get users from KA,IXL. Check for any schema upgrades
+# Get users from Kolibri and IXL (If centre gives literacy lessons)
 ~/.baseline_testing/scripts/start_users_extraction.sh
 
-#set up cron job to restart and repopulate users table on reboot
+# Set up cronjob to restart and repopulate users table on reboot
 ~/.baseline_testing/scripts/setup_cronjob.sh
 
-# Stop any running forever processes and start the server
-# assumes that node and forever located in /usr/local/bin
+# Stop all running forever processes and restart the server
 ~/.baseline_testing/scripts/start_testing_server.sh
