7a6b7fd325b34175b8bc6d0791af651f585369ae
Kapya <kapsakala@gmail.com>
2020-07-25 22:35:50 +0200

    added comments. removed logging statement on restart

--- scripts/start_testing_server.sh
+++ scripts/start_testing_server.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # Restart testing server
+
+# set vars for forever and node
 forever_path=/usr/local/bin/forever
 node_path=/usr/local/bin/node
 
+# stop all forever nodes
 $forever_path -c $node_path stopall > /dev/null
 
-cd ~/.baseline_testing
+# Change to the baseline directory
+cd ~/.baseline_testing || exit
+
+# start the main file in the application with forever
 $forever_path start -c $node_path index.js > /dev/null
-$forever_path -c $node_path logs 0 > /dev/null
+
+# show the first output of the logs
+# $forever_path -c $node_path logs 0 > /dev/null
