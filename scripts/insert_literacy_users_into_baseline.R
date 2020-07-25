894f091ad5f80e02a1cc3a24f9e26ab761d75f37
Kapya <kapsakala@gmail.com>
2020-07-25 23:58:35 +0200

    added comments

--- scripts/insert_literacy_users_into_baseline.R
+++ scripts/insert_literacy_users_into_baseline.R
@@ -19,14 +19,23 @@ bl_db_port = Sys.getenv("BASELINE_DATABASE_PORT")
 pg <- dbDriver("PostgreSQL")
 conn <-  dbConnect(pg, dbname= bl_db_name, host= bl_db_host, port= bl_db_port, user= bl_db_user, password= bl_db_passwd)
 
-# helper function to insert the contents of the users csv file into the users table on baseline
+# Insert the contents of the users csv file into the users table on baseline
 insert_responses <- function(input_file){
+	# Read the contents of the file supplied into a dataframe
 	users_df <- read.csv(input_file,stringsAsFactors = FALSE)
+
+	# Set the group name = "Lteracy"
 	users_df$group_name <- rep('Literacy',nrow(users_df))
+
+	# Write to the users table
 	dbWriteTable(conn,"users",users_df,append = TRUE, row.names = FALSE)
+
+	# Disconnect the database connection
 	dbDisconnect(conn)
 }
 
-input<- commandArgs(TRUE)
+# Capure arguments supplied in the command line
+input <- commandArgs(TRUE)
 
+# Call insert responses with the file path supplied in the command line
 insert_responses(input)
