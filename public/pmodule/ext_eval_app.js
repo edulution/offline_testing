0eda3eed32f88c40e79ddcbad2f67ca950fd723b
Kapya Sakala <kapsakala@gmail.com>
2021-05-05 16:15:09 +0200

    password for external evaluation day 4

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -53,7 +53,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
             })
 
             /*variables for validating coach_id*/
-            $scope.testSubmitPassword = "e3f2eb3d904e93a424a490f049c3d44e";
+            $scope.testSubmitPassword = "782dcf5a71b637ad21b89ac0182f118a";
             $scope.wrongPassword = false;
             $scope.wrongCoachID = false;
 
@@ -231,7 +231,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
     })
     /*Controller for password modal*/
     .controller('ModalInstanceCtrl', function($scope, $uibModalInstance, md5) {
-        $scope.coachPassword = "e3f2eb3d904e93a424a490f049c3d44e";
+        $scope.coachPassword = "782dcf5a71b637ad21b89ac0182f118a";
         $scope.wrongPassword = false;
 
         var $PasswordModalCtrl = this;
