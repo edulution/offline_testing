b2dbe0c77eef62f2d112c83c60af5d10244c2bd0
Kapya Sakala <kapsakala@gmail.com>
2021-05-10 16:56:48 +0200

    day 2 external evaluation

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -53,7 +53,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
             })
 
             /*variables for validating coach_id*/
-            $scope.testSubmitPassword = "2398228306c625c7aa239595a6cebbb9";
+            $scope.testSubmitPassword = "74aef121be35efccc4d308256e965805";
             $scope.wrongPassword = false;
             $scope.wrongCoachID = false;
 
@@ -231,7 +231,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
     })
     /*Controller for password modal*/
     .controller('ModalInstanceCtrl', function($scope, $uibModalInstance, md5) {
-        $scope.coachPassword = "2398228306c625c7aa239595a6cebbb9";
+        $scope.coachPassword = "74aef121be35efccc4d308256e965805";
         $scope.wrongPassword = false;
 
         var $PasswordModalCtrl = this;
