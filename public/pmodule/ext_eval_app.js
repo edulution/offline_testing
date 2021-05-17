2c8eef4549906f89544fbd32072ee11d34fa7cdf
Kapya Sakala <kapsakala@gmail.com>
2021-05-17 15:00:23 +0200

    updated password for brom ext_eval

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -53,7 +53,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
             })
 
             /*variables for validating coach_id*/
-            $scope.testSubmitPassword = "16ba5c386876deab9a0f44b15d005992";
+            $scope.testSubmitPassword = "fc11587579b9372463e406b065134af3";
             $scope.wrongPassword = false;
             $scope.wrongCoachID = false;
 
@@ -231,7 +231,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
     })
     /*Controller for password modal*/
     .controller('ModalInstanceCtrl', function($scope, $uibModalInstance, md5) {
-        $scope.coachPassword = "16ba5c386876deab9a0f44b15d005992";
+        $scope.coachPassword = "fc11587579b9372463e406b065134af3";
         $scope.wrongPassword = false;
 
         var $PasswordModalCtrl = this;
