581ce463c2a4ff3ca271183b528fb366e2109b21
Kapya Sakala <kapsakala@gmail.com>
2021-06-07 23:57:34 +0200

    function to get next course given course sequence and module

---
+++ migrations/V33__get_next_course.sql
@@ -0,0 +1,20 @@
+-- Create get_next course function
+-- Get the next course given sort order and module
+
+CREATE OR REPLACE FUNCTION get_next_course(
+	i_coursesequence integer,
+	i_module character varying)
+	RETURNS SETOF course
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+BEGIN
+RETURN QUERY
+SELECT *
+FROM course c
+WHERE c.sort_order::integer > i_coursesequence
+  AND c.module = i_module
+ORDER BY sort_order ASC LIMIT 1;
+END;
+$BODY$;
