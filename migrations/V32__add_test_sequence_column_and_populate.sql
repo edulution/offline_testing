6e0bb98c1eb6141c70cab586b6e741a5f6ddfd67
Kapya Sakala <kapsakala@gmail.com>
2021-06-07 23:56:03 +0200

    add test sequence column to test marks table and populate it

---
+++ migrations/V32__add_test_sequence_column_and_populate.sql
@@ -0,0 +1,152 @@
+/*Create a column for test sequence*/
+ALTER TABLE test_marks ADD COLUMN test_seq integer;
+
+/*Update test sequence for each test*/
+UPDATE test_marks
+SET test_seq=2
+WHERE test_id = 'level_1_sec_1';
+
+
+UPDATE test_marks
+SET test_seq=3
+WHERE test_id = 'level_1_sec_2';
+
+
+UPDATE test_marks
+SET test_seq=4
+WHERE test_id = 'level_1_sec_3';
+
+
+UPDATE test_marks
+SET test_seq=5
+WHERE test_id = 'level_1_sec_4';
+
+
+UPDATE test_marks
+SET test_seq=2
+WHERE test_id = 'level_2_sec_1';
+
+
+UPDATE test_marks
+SET test_seq=3
+WHERE test_id = 'level_2_sec_2';
+
+
+UPDATE test_marks
+SET test_seq=4
+WHERE test_id = 'level_2_sec_3';
+
+
+UPDATE test_marks
+SET test_seq=5
+WHERE test_id = 'level_2_sec_4';
+
+
+UPDATE test_marks
+SET test_seq=2
+WHERE test_id = 'level_3_sec_1';
+
+
+UPDATE test_marks
+SET test_seq=3
+WHERE test_id = 'level_3_sec_2';
+
+
+UPDATE test_marks
+SET test_seq=4
+WHERE test_id = 'level_3_sec_3';
+
+
+UPDATE test_marks
+SET test_seq=5
+WHERE test_id = 'level_3_sec_4';
+
+
+UPDATE test_marks
+SET test_seq=2
+WHERE test_id = 'level_4_sec_1';
+
+
+UPDATE test_marks
+SET test_seq=3
+WHERE test_id = 'level_4_sec_2';
+
+
+UPDATE test_marks
+SET test_seq=4
+WHERE test_id = 'level_4_sec_3';
+
+
+UPDATE test_marks
+SET test_seq=2
+WHERE test_id = 'level_5_sec_4';
+
+
+UPDATE test_marks
+SET test_seq=5
+WHERE test_id = 'level_4_sec_4';
+
+
+UPDATE test_marks
+SET test_seq=3
+WHERE test_id = 'level_5_sec_1';
+
+
+UPDATE test_marks
+SET test_seq=4
+WHERE test_id = 'level_5_sec_2';
+
+
+UPDATE test_marks
+SET test_seq=5
+WHERE test_id = 'level_5_sec_3';
+
+
+UPDATE test_marks
+SET test_seq=1
+WHERE test_id = 'level_1_pre';
+
+
+UPDATE test_marks
+SET test_seq=1
+WHERE test_id = 'level_2_pre';
+
+
+UPDATE test_marks
+SET test_seq=1
+WHERE test_id = 'level_3_pre';
+
+
+UPDATE test_marks
+SET test_seq=1
+WHERE test_id = 'level_4_pre';
+
+
+UPDATE test_marks
+SET test_seq=1
+WHERE test_id = 'level_5_pre';
+
+
+UPDATE test_marks
+SET test_seq=6
+WHERE test_id = 'level_1_post';
+
+
+UPDATE test_marks
+SET test_seq=6
+WHERE test_id = 'level_2_post';
+
+
+UPDATE test_marks
+SET test_seq=6
+WHERE test_id = 'level_3_post';
+
+
+UPDATE test_marks
+SET test_seq=6
+WHERE test_id = 'level_4_post';
+
+
+UPDATE test_marks
+SET test_seq=6
+WHERE test_id = 'level_5_post';
