e153de21a0fe89e493033b09c9bcb061f8fcada1
Kapya Sakala <kapsakala@gmail.com>
2021-07-27 15:13:45 +0200

    added score_pct and passed columns. improved formatting

--- migrations/V39__calc_test_score_fn.sql
+++ migrations/V39__calc_test_score_fn.sql
@@ -1,18 +1,46 @@
-CREATE OR REPLACE FUNCTION calc_test_score (
-	i_responseid uuid
-	)
+CREATE OR REPLACE FUNCTION calc_test_score ( i_responseid uuid )
 RETURNS TABLE(
+	user_id uuid,
 	response_id uuid,
-	score NUMERIC
+	test varchar,
+	course varchar,
+	module varchar,
+	testmaxscore integer,
+	test_pass_score numeric(3,2),
+	score NUMERIC,
+	score_pct numeric,
+	passed boolean
 	) as $$
 BEGIN
 	RETURN QUERY
-	-- Use dynamic query to coalesce and sum all of the q columns to derive the score
-    EXECUTE format('SELECT response_id, %s AS score FROM responses WHERE response_id = %s', string_agg(format('coalesce(%s::numeric, 0::numeric)', column_name), ' + '),quote_literal(i_responseid))
-    from information_schema.columns
-    where table_name = 'responses'
-    and column_name like 'q%';
-END;$$
+	-- Use dynamic query to coalesce all q columms to get the score
+	-- Join to test marks to get all other details about the test
+	-- Calculate the score percent and derive boolean of whether test was passed or not
+    EXECUTE format
+    ('SELECT *,
+       score/testmaxscore AS score_pct,
+       CASE
+           WHEN score >= testmaxscore * test_pass_score THEN TRUE
+           ELSE FALSE
+       END AS passed
+	FROM
+  		(SELECT user_id,
+          response_id,
+          r.test,
+          r.course,
+          r.module,
+          t.testmaxscore,
+          t.test_pass_score,
+          %s AS score
+   	FROM responses r
+   	LEFT JOIN test_marks t ON r.test::text = t.test_id::text
+   	AND r.course::text = t.course::text
+   	AND r.module::text = t.module::text
+   	WHERE response_id = %s)resp_detail',
+   string_agg(format('coalesce(%s::numeric, 0::numeric)', column_name), ' + '),quote_literal(i_responseid))    
+FROM information_schema.columns
+WHERE TABLE_NAME = 'responses'
+  AND COLUMN_NAME LIKE 'q%';
+ END;
+$$
 LANGUAGE plpgsql VOLATILE
-
-
