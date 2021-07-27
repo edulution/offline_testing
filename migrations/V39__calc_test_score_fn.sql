7fd92599f906d9ffb1febf6e4fcfdce9e24ada62
Kapya Sakala <kapsakala@gmail.com>
2021-07-27 14:18:47 +0200

    function to calulate the test score of a supplied response_id

---
+++ migrations/V39__calc_test_score_fn.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION calc_test_score (
+	i_responseid uuid
+	)
+RETURNS TABLE(
+	response_id uuid,
+	score NUMERIC
+	) as $$
+BEGIN
+	return query
+    EXECUTE format('SELECT response_id, %s AS score FROM responses WHERE response_id = %s', string_agg(format('coalesce(%s::numeric, 0::numeric)', column_name), ' + '),quote_literal(i_responseid))
+    from information_schema.columns
+    where table_name = 'responses'
+    and column_name like 'q%';
+END;$$
+LANGUAGE plpgsql VOLATILE
+
+
