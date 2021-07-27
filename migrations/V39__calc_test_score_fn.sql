1f89804101d313410decb1d4c62e6a0d9f1a7d8b
Kapya Sakala <kapsakala@gmail.com>
2021-07-27 14:20:20 +0200

    added comments

--- migrations/V39__calc_test_score_fn.sql
+++ migrations/V39__calc_test_score_fn.sql
@@ -6,7 +6,8 @@ RETURNS TABLE(
 	score NUMERIC
 	) as $$
 BEGIN
-	return query
+	RETURN QUERY
+	-- Use dynamic query to coalesce and sum all of the q columns to derive the score
     EXECUTE format('SELECT response_id, %s AS score FROM responses WHERE response_id = %s', string_agg(format('coalesce(%s::numeric, 0::numeric)', column_name), ' + '),quote_literal(i_responseid))
     from information_schema.columns
     where table_name = 'responses'
