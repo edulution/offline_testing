789e6b24f3956a725a840086b30d73879772d269
Kapya Sakala <kapsakala@gmail.com>
2021-07-31 13:29:57 +0200

    implemented recommended_test condition, interpolate as part of output when false is returned

--- migrations/R__user_testcheck.sql
+++ migrations/R__user_testcheck.sql
@@ -21,30 +21,81 @@ DECLARE
 highest_test_passed RECORD;
 
 has_written_currtest boolean;
+has_passed_currtest boolean;
 
-current_test record;
+current_test RECORD;
+
+recommended_test RECORD;
+
+recommended_course RECORD;
 
 can_write_test boolean;
+
 output_message text;
 
 BEGIN
   /*Get all details of the current test and store them in the current*/
+
   SELECT *
-FROM test_marks
-WHERE test_id = i_test
-  AND course = i_course
-  AND module = i_module INTO current_test;
+  FROM test_marks
+  WHERE test_id = i_test
+    AND course = i_course
+    AND module = i_module INTO current_test;
+
 
   /*Check if the user has already written the current test*/
   SELECT has_written_test(i_userid,i_test, i_course, i_module) INTO has_written_currtest;
+
+  /*Check if the user has already passed the current test*/
+  SELECT has_passed_test(i_userid,i_test, i_course, i_module) INTO has_passed_currtest;
+
+  /*Get the highest test the user has passed*/
+  SELECT * FROM get_highest_passed_test(i_userid, i_module) INTO highest_test_passed;
+
+  /*If the user has never passed any test, recommend the course with the lowest sort order in the module*/
+ IF highest_test_passed IS NULL THEN
+  SELECT *
+  FROM course
+  WHERE module = i_module
+    AND sort_order IS NOT NULL
+  ORDER BY sort_order LIMIT 1 INTO recommended_course;
+
+ ELSE
+  SELECT *
+  FROM get_next_course(highest_test_passed.sort_order, highest_test_passed.module) INTO recommended_course;
+ END IF;
+
+
+  /*Get the recommended_test*/
+
+  SELECT *
+  FROM get_tests_not_written(i_userid, i_module)
+  WHERE course = recommended_course.course
+  ORDER BY test_seq LIMIT 1 INTO recommended_test;
+
+
+--If a learner has written (and failed) all of the tests and testlets in a course, recommend the post-test
+ IF recommended_test IS NULL THEN
+  SELECT *
+  FROM test_marks
+  WHERE course = recommended_course.course
+    AND module = recommended_course.module
+  ORDER BY test_seq DESC LIMIT 1 INTO recommended_test;
+ END IF;
+
+
+
 
   -- If the current test is a pre_test and the user has written it before
   -- The user cannot write the test
   if current_test.test_type = 'TST' and current_test.test_id like '%_pre' and has_written_currtest = 't' then
     can_write_test := 'f';
-    output_message := 'A pre-test can only be written once';
-
-    end if;
+    output_message := 'A pre-test can only be written once. The recommended test is: ' || recommended_test.test_name;
+  /*If the user has already passed the current test, they cannot write it again*/
+  elsif has_passed_currtest = 't' then
+    can_write_test := 'f';
+    output_message := 'This test has already been passed. The recommended test is: '|| recommended_test.test_name;
+  end if;
 
 
 
