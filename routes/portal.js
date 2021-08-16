cc926c94f90a011bf540c8736872b41ce2de4021
Kapya Sakala <kapsakala@gmail.com>
2021-08-16 20:08:15 +0200

    added status code to all portal routes

--- routes/portal.js
+++ routes/portal.js
@@ -4,37 +4,37 @@ const path = require('path');
 
 /* Use the public directory to serve static assets*/
 router.use(express.static(path.resolve(
-    path.join(__basedir, 'public'),
+    path.join(__basedir, 'public')
 )));
 
 /* portal homepage*/
 router.get('/', (req, res) => {
-  res.sendFile(path.resolve('portal/index.html'));
+    res.status(200).sendFile(path.resolve('portal/index.html'));
 });
 
 /* numeracy assessments page*/
 router.get('/numeracy_tests', (req, res) => {
-  res.sendFile(path.resolve('portal/numeracy_tests.html'));
+    res.status(200).sendFile(path.resolve('portal/numeracy_tests.html'));
 });
 
 /* index of topics*/
 router.get('/topics', (req, res) => {
-  res.sendFile(path.resolve('portal/topics-index.html'));
+    res.status(200).sendFile(path.resolve('portal/topics-index.html'));
 });
 
 /* literacy assessments page*/
 router.get('/literacy_tests', (req, res) => {
-  res.sendFile(path.resolve('portal/literacy_tests.html'));
+    res.status(200).sendFile(path.resolve('portal/literacy_tests.html'));
 });
 
 /* grade7_revision page*/
 router.get('/grade7_revision', (req, res) => {
-  res.sendFile(path.resolve('portal/grade7_revision.html'));
+    res.status(200).sendFile(path.resolve('portal/grade7_revision.html'));
 });
 
 /* external evaluations page*/
 router.get('/external_evaluations', (req, res) => {
-  res.sendFile(path.resolve('portal/external_evaluations.html'));
+    res.status(200).sendFile(path.resolve('portal/external_evaluations.html'));
 });
 
 module.exports = router;
