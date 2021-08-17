63de18c360fd2de567eb7594a65bf81e908a90e4
Kapya Sakala <kapsakala@gmail.com>
2021-08-17 16:41:14 +0200

    added knex todos

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const url = require('url');
 
 const {Pool} = require('pg');
 
+/* TODO: replace with knex instatiation*/
 const pool = new Pool({
   user: process.env.BASELINE_DATABASE_USER,
   host: process.env.BASELINE_DATABASE_HOST,
@@ -52,6 +53,7 @@ router.get('/sucessful_submit', (request, response) => {
 /* endpoint to get users list as json*/
 router.get('/get_users', (request, response, next) => {
   const users_query = {
+    /* TODO: Replace with knex query*/
     /* Query to fetch all users*/
     name: 'fetch-users',
     text: 'SELECT * FROM users',
@@ -67,6 +69,7 @@ router.get('/get_users', (request, response, next) => {
 /* endpoint to get index_of_topics as json*/
 router.get('/index_of_topics', (request, response, next) => {
   const topics_query = {
+    /* TODO: Replace with knex query*/
     /* Query to fetch all topics*/
     name: 'fetch-topics',
     text: 'SELECT * FROM index_of_topics',
@@ -82,6 +85,8 @@ router.get('/index_of_topics', (request, response, next) => {
 /* endpoint to test count stats list as json*/
 router.get('/get_test_count', (request, response, next) => {
   const test_counts_query = {
+    /* TODO: Create view in database*/
+    /* TODO: Replace with knex query*/
     /* Query to count the number of tests and group by month end (last day(test date))*/
     name: 'fetch-test-counts',
     text: 'SELECT last_day(test_date::date) as test_month, count(*) as number_of_tests from responses group by last_day(test_date::date) order by last_day(test_date::date) desc',
@@ -97,6 +102,8 @@ router.get('/get_test_count', (request, response, next) => {
 /* endpoint to get all test_responses as json*/
 router.get('/get_responses', (request, response) => {
   const get_responses_query = {
+    /* TODO: Create view in database*/
+    /* TODO: Replace with knex query*/
     /* Query to fetch all the responses from the responses table and calculate the score percent for each one*/
     name: 'fetch-responses',
     text: 'select u.username,u.first_name,u.last_name,tm.test_name,r.*,round((coalesce(q1::integer,0.0)+ coalesce(q2::integer,0.0)+ coalesce(q3::integer,0.0)+ coalesce(q4::integer,0.0)+ coalesce(q5::integer,0.0)+ coalesce(q6::integer,0.0)+ coalesce(q7::integer,0.0)+ coalesce(q8::integer,0.0)+ coalesce(q9::integer,0.0)+ coalesce(q10::integer,0.0)+ coalesce(q11::integer,0.0)+ coalesce(q12::integer,0.0)+ coalesce(q13::integer,0.0)+ coalesce(q14::integer,0.0)+ coalesce(q15::integer,0.0)+ coalesce(q16::integer,0.0)+ coalesce(q17::integer,0.0)+ coalesce(q18::integer,0.0)+ coalesce(q19::integer,0.0)+ coalesce(q20::integer,0.0)+ coalesce(q21::integer,0.0)+ coalesce(q22::integer,0.0)+ coalesce(q23::integer,0.0)+ coalesce(q24::integer,0.0)+ coalesce(q25::integer,0.0)+ coalesce(q26::integer,0.0)+ coalesce(q27::integer,0.0)+ coalesce(q28::integer,0.0)+ coalesce(q29::integer,0.0)+ coalesce(q30::integer,0.0)+ coalesce(q31::integer,0.0)+ coalesce(q32::integer,0.0)+ coalesce(q33::integer,0.0)+ coalesce(q34::integer,0.0)+ coalesce(q35::integer,0.0)+ coalesce(q36::integer,0.0)+ coalesce(q37::integer,0.0)+ coalesce(q38::integer,0.0)+ coalesce(q39::integer,0.0)+ coalesce(q40::integer,0.0)+ coalesce(q41::integer,0.0)+ coalesce(q42::integer,0.0)+ coalesce(q43::integer,0.0)+ coalesce(q44::integer,0.0)+ coalesce(q45::integer,0.0)+ coalesce(q46::integer,0.0)+ coalesce(q47::integer,0.0)+ coalesce(q48::integer,0.0)+ coalesce(q49::integer,0.0)+ coalesce(q50::integer,0.0)+ coalesce(q51::integer,0.0)+ coalesce(q52::integer,0.0)+ coalesce(q53::integer,0.0)+ coalesce(q54::integer,0.0)+ coalesce(q55::integer,0.0)+ coalesce(q56::integer,0.0)+ coalesce(q57::integer,0.0)+ coalesce(q58::integer,0.0)+ coalesce(q59::integer,0.0)+ coalesce(q60::integer,0.0)+ coalesce(q61::integer,0.0)+ coalesce(q62::integer,0.0)+ coalesce(q63::integer,0.0)+ coalesce(q64::integer,0.0)+ coalesce(q65::integer,0.0)+ coalesce(q66::integer,0.0)+ coalesce(q67::integer,0.0)+ coalesce(q68::integer,0.0)+ coalesce(q69::integer,0.0)+ coalesce(q70::integer,0.0))/testmaxscore,2) as score_pct from responses r left join users u on r.user_id = u.user_id left join test_marks tm on r.test = tm.test_id and r.course = tm.course and r.module = tm.module order by test_date desc',
@@ -110,6 +117,7 @@ router.get('/get_responses', (request, response) => {
 
 router.get('/get_test_marks', (request, response) => {
   const test_marks_query = {
+    /* TODO: Replace with knex query*/
     name: 'fetch-test-marks',
     text: 'SELECT * from test_marks',
   };
@@ -173,7 +181,9 @@ router.post('/submit_test', [(request, response, next) => {
   console.log(uresponses);
 
   /* Insert statement to run on database. test date added as current date from server*/
-
+  /* TODO: Change table structure to make response columns jsonb*/
+  /* TODO: Check that jsonb can be inserted easily with knex*/
+  /* TODO: Replace with knex query*/
   const insert_statement = 'INSERT INTO responses(' + test_resp_props.toString() + ') values (' + uresponses_quoted + ')';
   console.log(insert_statement);
 
@@ -202,6 +212,7 @@ router.post('/overwrite_test', [(request, response, next) => {
   const test_module = test_resp.module;
   const test_date = test_resp.test_date;
 
+  /* TODO: Replace with knex query if possible*/
   /* Delete tests done by the same user in the same test, course, and module done on the same day*/
   const query_other = 'DELETE FROM responses where user_id=($1) and test=($2) and course=($3) and module=($4) and test_date=($5)';
   const values_other = [user_id, test_done, course, test_module, test_date];
@@ -264,7 +275,9 @@ router.post('/submit_ext_eval', (request, response, next) => {
   const uresponses_quoted = '\'' + uresponses.join('\',\'') + '\'';
 
   /* Insert statement to run on database. test date added as current date from server*/
-
+  /* TODO: Change table structure to make response columns jsonb*/
+  /* TODO: Check that jsonb can be inserted easily with knex*/
+  /* TODO: Replace with knex query*/
   const insert_statement = 'INSERT INTO ext_eval_responses(' + test_resp_props.toString() + ') values (' + uresponses_quoted + ')';
   console.log(insert_statement);
 
@@ -289,6 +302,7 @@ router.get('/user_testcheck', (request, response) => {
 
   /* declare a query variable containing a parametized call to the user_testcheck function*/
   const testcheck_query = {
+    /* TODO: Replace with knex query if possible*/
     /* Query to call function for test check*/
     name: 'check-user-test',
     text: 'SELECT * FROM user_testcheck($1,$2,$3,$4)',
