9fdf25773c0d8b393174e96b52e6206a99712952
Kapya Sakala <kapsakala@gmail.com>
2021-08-17 16:26:04 +0200

    resolved conflict between pretiffy and eslint. improved linting

--- routes/portal.js
+++ routes/portal.js
@@ -9,32 +9,32 @@ router.use(express.static(path.resolve(
 
 /* portal homepage*/
 router.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve('portal/index.html'));
+  res.status(200).sendFile(path.resolve('portal/index.html'));
 });
 
 /* numeracy assessments page*/
 router.get('/numeracy_tests', (req, res) => {
-    res.status(200).sendFile(path.resolve('portal/numeracy_tests.html'));
+  res.status(200).sendFile(path.resolve('portal/numeracy_tests.html'));
 });
 
 /* index of topics*/
 router.get('/topics', (req, res) => {
-    res.status(200).sendFile(path.resolve('portal/topics-index.html'));
+  res.status(200).sendFile(path.resolve('portal/topics-index.html'));
 });
 
 /* literacy assessments page*/
 router.get('/literacy_tests', (req, res) => {
-    res.status(200).sendFile(path.resolve('portal/literacy_tests.html'));
+  res.status(200).sendFile(path.resolve('portal/literacy_tests.html'));
 });
 
 /* grade7_revision page*/
 router.get('/grade7_revision', (req, res) => {
-    res.status(200).sendFile(path.resolve('portal/grade7_revision.html'));
+  res.status(200).sendFile(path.resolve('portal/grade7_revision.html'));
 });
 
 /* external evaluations page*/
 router.get('/external_evaluations', (req, res) => {
-    res.status(200).sendFile(path.resolve('portal/external_evaluations.html'));
+  res.status(200).sendFile(path.resolve('portal/external_evaluations.html'));
 });
 
 module.exports = router;
