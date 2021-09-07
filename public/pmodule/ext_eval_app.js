e0bc85c1af19cce2a5fbe7efbf984ac369327b51
Kapya Sakala <kapsakala@gmail.com>
2021-09-07 16:08:56 +0200

    password for testing independent evaluation test

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -53,7 +53,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
             })
 
             /*variables for validating coach_id*/
-            $scope.testSubmitPassword = "fc11587579b9372463e406b065134af3";
+            $scope.testSubmitPassword = "531d6870f7e438f7cf780112bd621dda";
             $scope.wrongPassword = false;
             $scope.wrongCoachID = false;
 
@@ -231,7 +231,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
     })
     /*Controller for password modal*/
     .controller('ModalInstanceCtrl', function($scope, $uibModalInstance, md5) {
-        $scope.coachPassword = "fc11587579b9372463e406b065134af3";
+        $scope.coachPassword = "531d6870f7e438f7cf780112bd621dda";
         $scope.wrongPassword = false;
 
         var $PasswordModalCtrl = this;
