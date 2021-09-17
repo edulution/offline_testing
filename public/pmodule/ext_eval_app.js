21f874a9a27b2ddc0e88c57e07892eed8d395595
Kapya Sakala <kapsakala@gmail.com>
2021-09-17 05:48:26 +0200

    Week 2 day 5 ext eval Namibia

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -53,7 +53,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
             })
 
             /*variables for validating coach_id*/
-            $scope.testSubmitPassword = "b7b294963da01a86a6e8fef80435b4e7";
+            $scope.testSubmitPassword = "d553a34c275612b6db8fa3535384ce2b";
             $scope.wrongPassword = false;
             $scope.wrongCoachID = false;
 
@@ -231,7 +231,7 @@ angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 
     })
     /*Controller for password modal*/
     .controller('ModalInstanceCtrl', function($scope, $uibModalInstance, md5) {
-        $scope.coachPassword = "b7b294963da01a86a6e8fef80435b4e7";
+        $scope.coachPassword = "d553a34c275612b6db8fa3535384ce2b";
         $scope.wrongPassword = false;
 
         var $PasswordModalCtrl = this;
