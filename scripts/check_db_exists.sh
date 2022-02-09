1639256903ec1f4eaa1b4e7b57e8bd61a5d5fb5f
ChisengaSol <solmanzambia@gmail.com>
2022-02-09 16:23:01 +0200

    added user, host and port to psql

--- scripts/check_db_exists.sh
+++ scripts/check_db_exists.sh
@@ -3,5 +3,5 @@
 # The kolibri database user can be used to check for other databases on the server too
 #!/bin/bash
 function db_exists() {
-  psql -lqt | cut -d \| -f 1 | grep -wq "$1"
+  PGPASSWORD=$BASELINE_DATABASE_PASSWORD psql -h "$BASELINE_DATABASE_HOST" -U "$BASELINE_DATABASE_USER" -lqt | cut -d \| -f 1 | grep -wq "$1"
 }
