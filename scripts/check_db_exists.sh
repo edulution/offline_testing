da536a861030be94c98d70d98e98914644840479
root <root@MunchyMuffin.localdomain>
2022-08-27 10:31:54 +0200

    Added help text to all bash scripts

--- scripts/check_db_exists.sh
+++ scripts/check_db_exists.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "NAME"
+  echo " check_db_exists - Check databases exit"
+  echo
+  echo "DESCRIPTION"
+  echo "  Using the Kolibri database user, the script checks if the Baseline database exists"
+  echo 
+  echo "Example"
+  echo " ./check_db_exists.sh "
+  exit 1
+fi
+
 # Function to check whether a database exists
 # The kolibri database user can be used to check for other databases on the server too
-#!/bin/bash
 function db_exists() {
   PGPASSWORD=$BASELINE_DATABASE_PASSWORD psql -h "$BASELINE_DATABASE_HOST" -U "$BASELINE_DATABASE_USER" -lqt | cut -d \| -f 1 | grep -wq "$1"
 }
