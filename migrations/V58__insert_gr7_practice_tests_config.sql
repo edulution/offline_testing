e480a79ee8badaa94990059d5a3f372315a8b290
Kapya Sakala <kapsakala@gmail.com>
2022-09-23 16:40:58 +0200

    added test type to insert of new practice tests config

--- migrations/V58__insert_gr7_practice_tests_config.sql
+++ migrations/V58__insert_gr7_practice_tests_config.sql
@@ -1,5 +1,5 @@
-INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, channel_id)
-VALUES('grade7_practice1', 'Grade 7 - Practice 1', 'zm_gr7_revision', 'numeracy',60, 0.75, NULL);
+INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, test_type, channel_id)
+VALUES('grade7_practice1', 'Grade 7 - Practice 1', 'zm_gr7_revision', 'numeracy',60, 0.75, 'EPR', NULL);
 
-INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, channel_id)
-VALUES('grade7_practice2', 'Grade 7 - Practice 2', 'zm_gr7_revision', 'numeracy',60, 0.75, NULL);
+INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, test_type, channel_id)
+VALUES('grade7_practice2', 'Grade 7 - Practice 2', 'zm_gr7_revision', 'numeracy',60, 0.75, 'EPR', NULL);
