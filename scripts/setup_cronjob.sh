cf8d86f3c34931ba4162ea2be9c1c43d8922f4b1
Ntipa Chola <sword@DESKTOP-NT5NC6Q>
2023-01-16 17:10:51 +0200

    Added help option to script

--- scripts/setup_cronjob.sh
+++ scripts/setup_cronjob.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "NAME"
+  echo "	setup_cronjob - Setup Cronjob"
+  echo
+  echo "DESCRIPTION"
+  echo "	Set up cronjob restarts and repopulates users table in baseline on reboot."
+  echo "	The script checks is cronjob exists for startup script and adds if cronjob does not exist."
+  echo
+  echo "Example"
+  echo "	./setup_cronjob.sh "
+  exit 1
+fi
+
 # check if cronjob exists for startup_script and add it if not
 # search for cronjob which matches @reboot ~/.baseline_testing/startup_script 
 if (crontab -l | grep -q '@reboot ~/.baseline_testing/scripts/startup_script'); then
