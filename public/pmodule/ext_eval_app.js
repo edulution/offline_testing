e5dd082707a222fd7db3c9a7114a0a0d0f6685c6
Kapya Sakala <kapsakala@gmail.com>
2023-02-07 07:25:20 +0200

    day 2 KZN eval

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -1,7 +1,7 @@
 /*Angular module to display password modal and make sure correct password is entered*/
 angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 'ui.filters', 'angular-md5'])
     .service('passwordService', function() {
-        var password = "c327bad2ed5cb158f1881bc139e5db37";
+        var password = "0460528997009c108a694c80831b7900";
 
         return {
             getPassword: function() {
