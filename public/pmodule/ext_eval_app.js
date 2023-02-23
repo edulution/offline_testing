7b0fcb2e84c6fbafbb808cac816f5d427adca2a9
Kapya Sakala <kapsakala@gmail.com>
2023-02-23 07:27:05 +0200

    day 4 MPL eval

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -1,7 +1,7 @@
 /*Angular module to display password modal and make sure correct password is entered*/
 angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 'ui.filters', 'angular-md5'])
     .service('passwordService', function() {
-        var password = "cc22d98c463e8edf7e2db572d1ea6c45";
+        var password = "697dd6c8691083e01069b1db1257c7dc";
 
         return {
             getPassword: function() {
