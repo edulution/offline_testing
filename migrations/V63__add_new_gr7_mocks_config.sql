88487dcf3ce1129dfb72d3a9794867efb8bbe76c
Kapya Sakala <kapsakala@gmail.com>
2024-04-16 11:23:38 +0200

    added migration for new gr7 mocks config

---
+++ migrations/V63__add_new_gr7_mocks_config.sql
@@ -0,0 +1,13 @@
+-- Config for new grade 7 mocks. Will be displayed as Mocks 1 to 4
+
+INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, test_type, channel_id)
+VALUES('grade7_mock5', 'Grade 7 - Mock 1', 'zm_gr7_revision', 'numeracy',60, 0.75, 'EPR', NULL);
+
+INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, test_type, channel_id)
+VALUES('grade7_mock6', 'Grade 7 - Mock 2', 'zm_gr7_revision', 'numeracy',60, 0.75, 'EPR', NULL);
+
+INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, test_type, channel_id)
+VALUES('grade7_mock7', 'Grade 7 - Mock 3', 'zm_gr7_revision', 'numeracy',60, 0.75, 'EPR', NULL);
+
+INSERT INTO test_marks(test_id, test_name, course, module, testmaxscore, test_pass_score, test_type, channel_id)
+VALUES('grade7_mock8', 'Grade 7 - Mock 4', 'zm_gr7_revision', 'numeracy',60, 0.75, 'EPR', NULL);
