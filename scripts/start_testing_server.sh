cc5664fa47afdc66869f953938d9cf7a1082cb39
Kapya Sakala <kapsakala@gmail.com>
2024-08-09 14:33:25 +0200

    call delete duplicate responses procedure when testing server is started

--- scripts/start_testing_server.sh
+++ scripts/start_testing_server.sh
@@ -9,7 +9,6 @@ node_path=/usr/local/bin/node
 # stop all forever nodes
 $forever_path -c $node_path stopall > /dev/null
 
-
 # Change to the baseline directory
 cd ~/.baseline_testing || exit
 
@@ -18,3 +17,6 @@ $forever_path start -c $node_path index.js > /dev/null
 
 # show the first output of the logs
 # $forever_path -c $node_path logs 0 > /dev/null
+
+# call a stored procedure to delete duplicate responses
+PGPASSWORD=$BASELINE_DATABASE_PASSWORD psql -h "$BASELINE_DATABASE_HOST" -U "$BASELINE_DATABASE_USER" -d "$BASELINE_DATABASE_NAME" -c "call delete_duplicate_responses();"
