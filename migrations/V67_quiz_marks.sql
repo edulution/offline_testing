33dd886cc71c26c0ac1cebb5e35cfe957f059442
Sword <ntch98@gmail.com>
2024-08-28 09:51:34 +0200

    🐛 Fix Bug - Updated insert table

--- migrations/V67_quiz_marks.sql
+++ migrations/V67_quiz_marks.sql
@@ -12,9 +12,9 @@ CREATE TABLE public.quiz_marks (
 INSERT INTO public.quiz_marks(
 	test_id, test_name, course, module, testmaxscore, channel_id)
 	VALUES ('grade_4_quiz','Grade 4 Math Quiz','math_quiz', 'numeracy', 19, NULL);
-INSERT INTO public.test_marks(
+INSERT INTO public.quiz_marks(
 	test_id, test_name, course, module, testmaxscore, channel_id)
 	VALUES ('grade_5_quiz','Grade 5 Math Quiz','math_quiz', 'numeracy', 15, NULL);
-INSERT INTO public.test_marks(
+INSERT INTO public.quiz_marks(
 	test_id, test_name, course, module, testmaxscore, channel_id)
 	VALUES ('grade_6_quiz','Grade 6 Math Quiz','math_quiz', 'numeracy', 30, NULL);
