1b5dc1061c17065b6c0ff67d92d4e678a5a4ec30
Kapya Sakala <kapsakala@gmail.com>
2024-12-05 11:34:58 +0200

    remove cols from external table, causing error when running spassignmembership

---
+++ migrations/V66__remove_cols_from_foreign_table.sql
@@ -0,0 +1,15 @@
+# Remove these columns
+FROM
+    the FOREIGN TABLE ext.kolibriauth collection due TO error caused
+    WHEN running spassignmembership ALTER FOREIGN TABLE ext.kolibriauth_collection
+            DROP COLUMN IF EXISTS lft;
+
+ALTER FOREIGN TABLE ext.kolibriauth_collection
+    DROP COLUMN IF EXISTS rght;
+
+ALTER FOREIGN TABLE ext.kolibriauth_collection
+    DROP COLUMN IF EXISTS tree_id;
+
+ALTER FOREIGN TABLE ext.kolibriauth_collection
+    DROP COLUMN IF EXISTS level;
+
