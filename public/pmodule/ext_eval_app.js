84ec79603ab9dcccd12e860950fca25a7d86f516
Dabwitso Mweemba <mweembadabwitso@gmail.com>
2025-02-18 23:06:17 +0200

    ZM I.E Password

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -1,7 +1,7 @@
 /*Angular module to display password modal and make sure correct password is entered*/
 angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 'ui.filters', 'angular-md5'])
     .service('passwordService', function() {
-        var password = "e49cc6fba5a74f00d6626181176e8372";
+        var password = "988fec27a96f60d1ba2ec63b5a38ddc3";
 
         return {
             getPassword: function() {
