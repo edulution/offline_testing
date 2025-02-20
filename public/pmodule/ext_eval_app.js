f0a0960f11fdb50ae3cc4b084371054988caf462
Sword <61139457+ntipac@users.noreply.github.com>
2025-02-20 22:08:39 +0200

    Updated IE hash 210225

--- public/pmodule/ext_eval_app.js
+++ public/pmodule/ext_eval_app.js
@@ -1,7 +1,7 @@
 /*Angular module to display password modal and make sure correct password is entered*/
 angular.module('passProtect', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui', 'ui.filters', 'angular-md5'])
     .service('passwordService', function() {
-        var password = "af21918c950501019108afe356c33e15";
+        var password = "3ec9ce248bf1e9238d92484715b72a97";
 
         return {
             getPassword: function() {
