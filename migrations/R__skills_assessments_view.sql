1b37a1c21a13a5ddeb1bf890e8ae62a6963cd521
Sword <ntch98@gmail.com>
2025-05-01 11:42:50 +0200

    🐛 Fix Bug - Updated table reference

--- migrations/R__skills_assessments_view.sql
+++ migrations/R__skills_assessments_view.sql
@@ -1,5 +1,5 @@
-create or replace view vskillsscores as
-WITH skillshub_scores_raw AS (
+create or replace view vquizscores as
+WITH quiz_scores_raw AS (
     SELECT
         response_id,
         user_id,
@@ -7,10 +7,9 @@ WITH skillshub_scores_raw AS (
         course,
         module,
         test_date,
-        (COALESCE(q1::int, 0) + COALESCE(q2::int, 0) + COALESCE(q3::int, 0) + COALESCE(q4::int, 0) + COALESCE(q5::int, 0) + COALESCE(q6::int, 0) + COALESCE(q7::int, 0) + COALESCE(q8::int, 0) + COALESCE(q9::int, 0) + COALESCE(q10::int, 0) + COALESCE(q11::int, 0) + COALESCE(q12::int, 0) + COALESCE(q13::int, 0) + COALESCE(q14::int, 0) + COALESCE(q15::int, 0) + COALESCE(q16::int, 0) + COALESCE(q17::int, 0) + COALESCE(q18::int, 0) + COALESCE(q19::int, 0) + COALESCE(q20::int, 0) + COALESCE(q21::int, 0) + COALESCE(q22::int, 0) + COALESCE(q23::int, 0) + COALESCE(q24::int, 0) + COALESCE(q25::int, 0) + COALESCE(q26::int, 0) + COALESCE(q27::int, 0) + COALESCE(q28::int, 0) + COALESCE(q29::int, 0) + COALESCE(q30::int, 0)) AS score,
-        testmaxscore
+        (COALESCE(q1::int, 0) + COALESCE(q2::int, 0) + COALESCE(q3::int, 0) + COALESCE(q4::int, 0) + COALESCE(q5::int, 0) + COALESCE(q6::int, 0) + COALESCE(q7::int, 0) + COALESCE(q8::int, 0) + COALESCE(q9::int, 0) + COALESCE(q10::int, 0) + COALESCE(q11::int, 0) + COALESCE(q12::int, 0) + COALESCE(q13::int, 0) + COALESCE(q14::int, 0) + COALESCE(q15::int, 0) + COALESCE(q16::int, 0) + COALESCE(q17::int, 0) + COALESCE(q18::int, 0) + COALESCE(q19::int, 0) + COALESCE(q20::int, 0) + COALESCE(q21::int, 0) + COALESCE(q22::int, 0) + COALESCE(q23::int, 0) + COALESCE(q24::int, 0) + COALESCE(q25::int, 0) + COALESCE(q26::int, 0) + COALESCE(q27::int, 0) + COALESCE(q28::int, 0) + COALESCE(q29::int, 0) + COALESCE(q30::int, 0)) AS score
     FROM
-        public.skillshub_marks
+        public.skillshub_quiz
 )
 SELECT
     response_id,
@@ -23,7 +22,7 @@ SELECT
     round(score::numeric / testmaxscore::numeric, 2) AS score_pct,
     test_date
 FROM
-    skillshub_scores_raw m
+    quiz_scores_raw m
     LEFT JOIN users u ON m.user_id = u.user_id
     LEFT JOIN skillshub_marks q ON m.test = q.test_id
         AND m.course = q.course
