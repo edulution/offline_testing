f5f7c8dbda5ba2191a8b4470a2ca7c16e1924992
Sword <ntch98@gmail.com>
2025-05-01 09:00:29 +0200

    🐛 Fix Bug - Split migration

--- migrations/V67_create_skillshub_assessment_table.sql
+++ migrations/V67_create_skillshub_assessment_table.sql
@@ -6,12 +6,3 @@ CREATE TABLE public.skillshub_marks (
     module varchar NOT NULL,
 	testmaxscore varchar
 );
-
-
--- Insert test marks for skillshub_assessments  
-INSERT INTO public.skillshub_marks(
-	test_id, test_name, course, module, testmaxscore)
-	VALUES ('literacy_assessment','Literacy Assessment','literacy_quiz', 'literacy', 20);
-INSERT INTO public.skillshub_marks(
-	test_id, test_name, course, module, testmaxscore)
-	VALUES ('math_assessment','Math Assessment','math_quiz', 'numeracy', 20);
