c859b3e660378d6bbef2be6c53979bb560323157
Sword <ntch98@gmail.com>
2025-05-02 09:16:58 +0200

    🔨 Refactor code - updated routes

--- routes/skills_hub.js
+++ routes/skills_hub.js
@@ -1,25 +1,46 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+
+const router = express.Router();
+
 const path = require('path');
 
-/*Use the public directory to serve static assets*/
+
+
+/* Use the public directory to serve static assets */
+
 router.use(express.static(path.resolve(
+
     path.join(__basedir, 'public')
-)))
 
-/*skills hub assessments page*/
-router.get('/', (req, res) => {
-    res.sendFile(path.resolve('skills_hub/index.html'));
+)));
+
+
+
+/* Get the selected test as a param of the request */
+
+/* then construct the path to the html file */
+
+/* It is assumed that the name of the html file is the same as the requested test */
+
+router.get('/:quiz', (req, res) => {
+
+    let selected_module = "skills_hub";
+
+
+    let selected_test = req.params.quiz;
+
+
+
+    /* Construct path to the selected test in the math_quiz directory */
+
+    res.sendFile(path.resolve(
+
+        path.join(selected_module, selected_test + '.html')
+
+    ));
+
 });
 
-/*skills hub math assessment*/
-router.get('/math_assessment', (req, res) => {
-    res.sendFile(path.resolve('skills_hub/math_assessment.html'));
-});
 
-/*skills hub literacy assessment*/
-router.get('/literacy_assessment', (req, res) => {
-    res.sendFile(path.resolve('skills_hub/literacy_assessment.html'));
-});
 
-module.exports = router 
+module.exports = router;
