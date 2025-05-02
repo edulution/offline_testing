874c7ac68846cfe50fdbac2a95625ba85f7a9996
Sword <ntch98@gmail.com>
2025-05-02 14:06:41 +0200

    🐛 Fix Bug - updated test ids

--- migrations/V68__skillshub_courses.sql
+++ migrations/V68__skillshub_courses.sql
@@ -2,5 +2,5 @@
 INSERT INTO public.skillshub_marks(
     test_id, test_name, course, module, testmaxscore)
 VALUES 
-    ('literacy_assessment','Literacy Assessment','literacy_quiz', 'literacy', 20),
-    ('math_assessment','Math Assessment','math_quiz', 'numeracy', 20);
+    ('skills_hub_literacy_assessment','Literacy Assessment','literacy_quiz', 'literacy', 20),
+    ('skills_hub_math_assessment','Math Assessment','math_quiz', 'numeracy', 20);
