af106773d980e23a53402d0d4d4e602282b537d2
Sword <ntch98@gmail.com>
2025-05-02 15:10:15 +0200

    🐛 Fix Bug - added root path handler

--- routes/skills_hub.js
+++ routes/skills_hub.js
@@ -16,21 +16,29 @@ router.use(express.static(path.resolve(
 
 
 
+/* Handle root path to serve index.html */
+router.get('/', (req, res) => {
+    const indexPath = path.resolve(
+        path.join(__basedir, 'skills_hub', 'index.html')
+    );
+
+    if (!require('fs').existsSync(indexPath)) {
+        return res.status(404).send('Skills Hub index not found');
+    }
+
+    res.sendFile(indexPath);
+});
+
 /* Get the selected test as a param of the request */
-
 /* then construct the path to the html file */
-
 /* It is assumed that the name of the html file is the same as the requested test */
-
 router.get('/:quiz', (req, res) => {
-
     let selected_module = "skills_hub";
-
     let selected_test = req.params.quiz;
 
     /* Construct path to the selected test in the skills_hub directory */
     const filePath = path.resolve(
-        path.join(__basedir, selected_module, selected_test + '.html')
+        path.join(__basedir, selected_module, selected_test)
     );
 
     // Check if file exists before sending
